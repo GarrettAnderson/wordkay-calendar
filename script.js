7c99af857f5e58fc741d1d78ea0a50f4d54cf992
Garrett <garrettleegrahamanderson@gmail.com>
2022-12-10 18:11:36 -0500

    removed the comment in the index html and the code blocks for the past present and future times. removed the empty function in the script file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // in the html.
 
 $(document).ready(function() {
-  $(function () {
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -23,7 +23,7 @@ $(document).ready(function() {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+
 
 
 
